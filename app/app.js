60db77fc92d8515e0d3e67ce9142c97f47ea5092
Peter McIntyre <peter.mcintyre+github@gmail.com>
2015-03-02 23:17:11 +1100

    messy, but does a bit more
    dont look at this

--- app/app.js
+++ app/app.js
@@ -1,60 +1,141 @@
 'use strict';
 
 var chairFirebaseUrl = 'https://chair.firebaseio.com/';
-var fireRef = new Firebase(chairFirebaseUrl)
+var chairFirebaseUrls = {
+	base: chairFirebaseUrl,
+	auth: chairFirebaseUrl+'/auth',
+	people: chairFirebaseUrl+'/people',
+	meetings: chairFirebaseUrl+'/meetings',
+	meta: chairFirebaseUrl+'/meta'
+}
+var fireRef = new Firebase(chairFirebaseUrl);
 
-angular.module('chairApp', ['ngRoute','firebase'])
-.controller('homeCtrl', function ($scope) {
+var myId = null;
 
+angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
+.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
+	var ref = new Firebase(chairFirebaseUrls.auth);
+	return $firebaseAuth(ref);
+}])
+.factory('IdentityService', function ($cookieStore, $firebase) {
+	
+	var id = $cookieStore.get('id') || null;
+	var me = {
+		name : $cookieStore.get('name') || '',
+		time : $cookieStore.get('time') || null
+	}
+
+	var sync = function () {
+		if (id) {
+			var sync = $firebase(new Firebase(chairFirebaseUrls.people).child(id));
+			var temp = sync.$asObject();
+
+			if (!temp.time || me.time > temp.time) {
+				temp.name = me.name;
+				temp.time = me.time;
+				temp.$save();
+				me = temp;
+			} else {
+				me = temp;
+			}
+		} else {
+			$firebase(new Firebase(chairFirebaseUrls.people))
+			.$push(me).then( function (ref) {
+				id = ref.key();
+				$cookieStore.put('id',id);
+			})
+		}
+		$cookieStore.put('name',me.name);
+		$cookieStore.put('time',me.time);
+	}
+	sync();
+
+	var functions = {
+		get: function () {
+			return me;
+		},
+		set: function (n) {
+			console.log('persisting name');
+
+			me.name = n;
+			me.time = new Date().getTime();
+
+			sync();
+		},
+		getId: function () { return id }
+	}
+	return functions;
 })
-.controller('chairCtrl', function ($scope) {
+.controller('credentialCtrl', function ($scope, IdentityService) {
+	$scope.me = IdentityService.get();
+	$scope.update = function(){
+		IdentityService.set($scope.me.name);
+	}
+})
+.controller('homeCtrl', function ($scope, $firebase) {
+	// $firebase(new Firebase(chairFirebaseUrls.people))
+	// .$push({name: 'unknown'})
+	// .then(function(ref){
+	// 	// TODO: save ref into cookie
+	// 	myId = ref.key();
+	// 	$cookieStore.put(myId,null);
+	// });
+})
+.controller('chairCtrl', function ($scope, $firebase, IdentityService) {
 	// $scope.randomMeetingId = randomString(5);
-	new Firebase(chairFirebaseUrl).push({
-		time: (new Date().getTime()),
-		attendance: true
-	}).then(function(newChildRef) {
-		$scope.randomMeetingId = newChildRef.key();
-	  console.log("added record with id " + newChildRef.key());
-	});;
+	var sync = $firebase(new Firebase(chairFirebaseUrl));
+
+	// Create a meeting
+	$firebase(new Firebase(chairFirebaseUrls.meetings)).$push().then(function(ref){
+		$scope.randomMeetingId = ref.key();
+
+		// save some meta data
+		$firebase(new Firebase(chairFirebaseUrls.meta)).$push({
+			time:(new Date().getTime()),
+			chair: IdentityService.get() || 'unknownchair'
+		}).then(function(ref){
+			
+		});
+	});
+
+	// sync.$push({
+	// 	time: (new Date().getTime()),
+	// 	attendance: []
+	// }).then(function(newChildRef) {
+	// 	$scope.randomMeetingId = newChildRef.key();
+	// 	console.log("added record with id " + newChildRef.key());
+	// });;
 
 })
-.controller('meetingCtrl', function ($scope, $routeParams, $firebase) {
+.controller('meetingCtrl', function ($scope, $routeParams, $firebase, IdentityService) {
 	$scope.meetingId = $routeParams.meetingId;
 	$scope.currentUrl = encodeURIComponent(window.location);
 
-	$scope.me = {
-		name: '',
-		id: null
-	}
+	$firebase(new Firebase(chairFirebaseUrls.meetings).child($scope.meetingId))
+	.$asObject()
+	.$bindTo($scope, "attendance");
 
-	var meeetingFireRef = new Firebase(chairFirebaseUrl).child($scope.meetingId);
-
-	// create an AngularFire reference to the data
-	var sync = $firebase(meeetingFireRef);
-
-	// download the data into a local object
-	// var syncObject = sync.$asObject();
-	// syncObject.$bindTo($scope, "data");
-
-	var meeetingFireRef = sync.$asObject();
-	$scope.meeting = meeetingFireRef;
+	$scope.push(IdentityService.getId());
 
 	$scope.attend = function (){
 
-		console.log(meeetingFireRef);
-		console.log(meeetingFireRef.attendance);
-		console.log(sync);
-		console.log(sync.attendance);
-		console.log(sync.child('attendance'));
-		// console.log(meeetingFireRef.child('attendance'));
-
-		sync.attendance.$push({
+		$firebase(new Firebase(chairFirebaseUrls.meetings).child($scope.meetingId).child(myId))
+		.$set({
 			name: $scope.me.name,
 			time: (new Date().getTime())
-		}).then(function(newChildRef) {
-			$scope.me.id = newChildRef.key();
-		  console.log("added record with id " + newChildRef.key());
+		})
+		.then(function(ref){
+			// TODO: save ref into cookie
+			// myId = ref.key();
 		});
+
+		// sync.attendance.$push({
+		// 	name: $scope.me.name,
+		// 	time: (new Date().getTime())
+		// }).then(function(newChildRef) {
+		// 	$scope.me.id = newChildRef.key();
+		//   console.log("added record with id " + newChildRef.key());
+		// });
 	};
 
 })
