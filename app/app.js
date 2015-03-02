489fdea7053c60f0d5d84c157c62abd3eaded4d1
Peter McIntyre <peter.mcintyre+github@gmail.com>
2015-03-03 00:15:30 +1100

    Getting there

--- app/app.js
+++ app/app.js
@@ -20,57 +20,17 @@ angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
 .factory('IdentityService', function ($cookieStore, $firebase) {
 	
 	var id = $cookieStore.get('id') || null;
-	var me = {
-		name : $cookieStore.get('name') || '',
-		time : $cookieStore.get('time') || null
+	if (!id) {
+		$firebase(new Firebase(chairFirebaseUrls.people)).$push({name:''}).then(function(ref){
+			$cookieStore.put('id',ref.key());
+		})
 	}
 
-	var sync = function () {
-		if (id) {
-			var sync = $firebase(new Firebase(chairFirebaseUrls.people).child(id));
-			var temp = sync.$asObject();
-
-			if (!temp.time || me.time > temp.time) {
-				temp.name = me.name;
-				temp.time = me.time;
-				temp.$save();
-				me = temp;
-			} else {
-				me = temp;
-			}
-		} else {
-			$firebase(new Firebase(chairFirebaseUrls.people))
-			.$push(me).then( function (ref) {
-				id = ref.key();
-				$cookieStore.put('id',id);
-			})
-		}
-		$cookieStore.put('name',me.name);
-		$cookieStore.put('time',me.time);
-	}
-	sync();
-
-	var functions = {
-		get: function () {
-			return me;
-		},
-		set: function (n) {
-			console.log('persisting name');
-
-			me.name = n;
-			me.time = new Date().getTime();
-
-			sync();
-		},
-		getId: function () { return id }
-	}
-	return functions;
+	var sync = $firebase(new Firebase(chairFirebaseUrls.people).child(id));
+	return sync.$asObject();
 })
 .controller('credentialCtrl', function ($scope, IdentityService) {
-	$scope.me = IdentityService.get();
-	$scope.update = function(){
-		IdentityService.set($scope.me.name);
-	}
+	IdentityService.$bindTo($scope, "me");
 })
 .controller('homeCtrl', function ($scope, $firebase) {
 	// $firebase(new Firebase(chairFirebaseUrls.people))
@@ -82,27 +42,27 @@ angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
 	// });
 })
 .controller('chairCtrl', function ($scope, $firebase, IdentityService) {
-	// $scope.randomMeetingId = randomString(5);
+	// $scope.meetingId = randomString(5);
 	var sync = $firebase(new Firebase(chairFirebaseUrl));
 
 	// Create a meeting
-	$firebase(new Firebase(chairFirebaseUrls.meetings)).$push().then(function(ref){
-		$scope.randomMeetingId = ref.key();
+	$firebase(new Firebase(chairFirebaseUrls.meetings)).$push({}).then(function(ref){
+		$scope.meetingId = ref.key();
 
 		// save some meta data
-		$firebase(new Firebase(chairFirebaseUrls.meta)).$push({
-			time:(new Date().getTime()),
-			chair: IdentityService.get() || 'unknownchair'
-		}).then(function(ref){
+		// $firebase(new Firebase(chairFirebaseUrls.meta)).$push({
+		// 	time:(new Date().getTime()),
+		// 	chair: IdentityService.get() || 'unknownchair'
+		// }).then(function(ref){
 			
-		});
+		// });
 	});
 
 	// sync.$push({
 	// 	time: (new Date().getTime()),
 	// 	attendance: []
 	// }).then(function(newChildRef) {
-	// 	$scope.randomMeetingId = newChildRef.key();
+	// 	$scope.meetingId = newChildRef.key();
 	// 	console.log("added record with id " + newChildRef.key());
 	// });;
 
@@ -111,11 +71,16 @@ angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
 	$scope.meetingId = $routeParams.meetingId;
 	$scope.currentUrl = encodeURIComponent(window.location);
 
-	$firebase(new Firebase(chairFirebaseUrls.meetings).child($scope.meetingId))
-	.$asObject()
-	.$bindTo($scope, "attendance");
+	$scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings).child($scope.meetingId))
+	.$asArray();
 
-	$scope.push(IdentityService.getId());
+	$scope.me = IdentityService;
+	$scope.me.$loaded( function (data){
+		$firebase(new Firebase(chairFirebaseUrls.meetings).child($scope.meetingId).child(data.$id)).$set({name:data.name});
+		// $scope.attendance.$add(data);
+		
+	});
+
 
 	$scope.attend = function (){
 
