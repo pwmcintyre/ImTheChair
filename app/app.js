91677ecfc0c8d527099f3dd413f7ffdf01db7aef
Peter McIntyre <contactpeter1@gmail.com>
2015-03-09 22:51:56 +1100

    updated angularfire

--- app/app.js
+++ app/app.js
@@ -17,62 +17,111 @@ angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
 .factory('IdentityService', function ($cookieStore){
 	return {
     get : function() {
-        return $cookieStore.get('name') || '';
+        return $cookieStore.get('user') || '';
     },
-    set : function(n) {
-        $cookieStore.put('name',n);
+    set : function(u) {
+        $cookieStore.put('user',u);
     }
   }
 })
-.controller('homeCtrl', function ($scope) {
-	$scope.user;
-	$scope.meetingId;
-})
-.controller('credentialCtrl', function ($scope, $cookieStore) {
+.controller('homeCtrl', function ($scope, $routeParams) {
 	$scope.user = {
-	  name: function(newName) {
-	    if (angular.isDefined(newName)) {
-	      $cookieStore.put('name',n);
-	    }
-	    return $cookieStore.get('name') || '';
-	  }
+		id: '',
+		name: '',
+		ref: {
+			$value: ''
+		}
+	};
+	$scope.meeting = {
+		id: $routeParams.meetingId,
+		attendance: []
 	};
 })
-.controller('chairCtrl', function ($scope, $firebase, $location) {
+.controller('credentialCtrl', function ($scope, $cookieStore) {
+	// $scope.user.id = $cookieStore.get('user').id;
+	// $scope.user.name = $cookieStore.get('user').name;
+	// $scope.user.ref = $cookieStore.get('user').ref;
+
+	var cookie = angular.fromJson($cookieStore.get('user'))
+	$scope.user.name = cookie.name;
+
+	$scope.setUser = function () {
+		$cookieStore.put('user', angular.toJson($scope.user) );
+	}
+})
+.controller('chairCtrl', function ($scope, $location) {
 
 	$scope.newMeeting = function () {
-		// $scope.meetingId = randomString(5);
-		var sync = $firebase(new Firebase(chairFirebaseUrl));
+		$scope.meeting.id = randomString(5);
+		// var sync = $firebase(new Firebase(chairFirebaseUrl));
 
 		// Create a meeting
-		$firebase(new Firebase(chairFirebaseUrls.meetings)).$push({}).then(function(ref){
-			$scope.meetingId = ref.key();
-			$location.path($location.path()+'/'+$scope.meetingId );
-		});
+		// var ref = new Firebase(chairFirebaseUrls.meetings);
+		// $firebase(new Firebase(chairFirebaseUrls.meetings)).$push({}).then(function(ref){
+		// 	$scope.meeting.id = ref.key();
+		// 	// $location.path($location.path()+'/'+$scope.meetingId );
+		// });
 	}
-	
+	$scope.newMeeting();
+
 })
-.controller('meetingCtrl', function ($scope, $routeParams, $firebase, $cookieStore, IdentityService) {
-	$scope.meetingId = $routeParams.meetingId;
+.controller('meetingCtrl', function ($scope, $routeParams, $firebaseObject, $firebaseArray, $cookieStore, IdentityService) {
+	$scope.meeting.id = $routeParams.meetingId;
 	$scope.currentUrl = encodeURIComponent(window.location);
+	
+	console.log($routeParams.meetingId);
 
-	$scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings).child($routeParams.meetingId))
-	.$asArray();
+	var meetingUrl = chairFirebaseUrls.meetings + '/' + $routeParams.meetingId;
+	var meetingRef = new Firebase(meetingUrl);
+	$scope.attendance = $firebaseArray(meetingRef);
+	
+	// $scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings)
+	// 	.child($routeParams.meetingId))
+	// 	.$asArray();
 
-	$scope.attendance.$add($scope.user);
-	$cookieStore.put('name',$scope.user);
+	$scope.me;
+	$scope.attend = function() {
 
+		// console.log($scope.me);
+
+		// if (!$scope.me) {
+
+			// Add me to the meeting
+			$scope.attendance.$add($scope.user).then(function(ref) {
+
+				console.log(ref);
+
+				// Save the reference
+				$scope.me = ref;
+				// $scope.user.id = ref.key();
+
+				var userUrl = meetingUrl + '/' + ref.key();
+				var userRef = new Firebase(userUrl);
+				var obj = $firebaseObject(userRef);
+				obj.$bindTo($scope, "user").then(function(ref) {
+					console.log(ref);
+				  // console.log($scope.data); // { foo: "bar" }
+				  // $scope.data.foo = "baz";  // will be saved to Firebase
+				  // ref.set({ foo: "baz" });  // this would update Firebase and $scope.data
+				});
+			});
+		// }
+	};
+	// $scope.attend = function() {
+	// 	if (!$scope.me)
+	// 		$scope.attendance.$add($scope.user.name).then(function(ref) {
+	// 			$scope.me = ref;
+	// 			$scope.user.id = ref.key();
+	// 			// $scope.setUser();
+	// 		});
+	// }
 })
 .controller('roomCtrl', function ($scope, $routeParams, $firebase, $cookieStore, IdentityService) {
-	$scope.meetingId = $routeParams.meetingId;
+	$scope.meeting.id = $routeParams.meetingId;
 	$scope.currentUrl = encodeURIComponent(window.location);
 
 	$scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings).child($routeParams.meetingId))
 	.$asArray();
-
-	$scope.attendance.$add($scope.user);
-	$cookieStore.put('name',$scope.user);
-
 })
 .config(['$routeProvider',
   function($routeProvider) {
@@ -84,6 +133,10 @@ angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
       when('/chair', {
         templateUrl: 'partials/chair.html',
         controller: 'chairCtrl'
+      }).
+      when('/:meetingId', {
+        templateUrl: 'partials/meeting.html',
+        controller: 'meetingCtrl'
       }).
       when('/meeting/:meetingId', {
         templateUrl: 'partials/meeting.html',
