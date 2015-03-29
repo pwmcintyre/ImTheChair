fd972b3c5e75fb78af1c501dafa6fb60857c617a
Peter McIntyre <contactpeter1@gmail.com>
2015-03-30 02:20:07 +1100

    Time for a big ol' rework

--- app/app.js
+++ app/app.js
@@ -14,151 +14,88 @@ var myId = null;
 
 angular.module('chairApp', ['ngRoute','firebase','ngCookies'])
 
-.factory('IdentityService', function ($cookieStore){
+.factory('meService', function ($cookieStore){
+	var name = $cookieStore.get('name') || 'noname';
 	return {
-    get : function() {
-        return $cookieStore.get('user') || '';
-    },
-    set : function(u) {
-        $cookieStore.put('user',u);
-    }
-  }
-})
-.controller('homeCtrl', function ($scope, $routeParams) {
-	$scope.user = {
-		id: '',
-		name: '',
-		ref: {
-			$value: ''
+		name : function(n) {
+			if (n) {
+				name = n;
+				$cookieStore.put('name',n);
+			}
+			return name;
 		}
-	};
-	$scope.meeting = {
-		id: $routeParams.meetingId,
-		attendance: []
-	};
-})
-.controller('credentialCtrl', function ($scope, $cookieStore) {
-	// $scope.user.id = $cookieStore.get('user').id;
-	// $scope.user.name = $cookieStore.get('user').name;
-	// $scope.user.ref = $cookieStore.get('user').ref;
-
-	var cookie = angular.fromJson($cookieStore.get('user'))
-	$scope.user.name = cookie.name;
-
-	$scope.setUser = function () {
-		$cookieStore.put('user', angular.toJson($scope.user) );
 	}
 })
-.controller('chairCtrl', function ($scope, $location) {
+.factory('meetingService', function ($firebaseObject, $firebaseArray){
 
-	$scope.newMeeting = function () {
-		$scope.meeting.id = randomString(5);
-		// var sync = $firebase(new Firebase(chairFirebaseUrl));
+	var meetingId = null;
+	var meetingRef = null;
 
-		// Create a meeting
-		// var ref = new Firebase(chairFirebaseUrls.meetings);
-		// $firebase(new Firebase(chairFirebaseUrls.meetings)).$push({}).then(function(ref){
-		// 	$scope.meeting.id = ref.key();
-		// 	// $location.path($location.path()+'/'+$scope.meetingId );
-		// });
+	return {
+		get : function(m) {
+			meetingId = m || randomString(10);
+			var meetingUrl = chairFirebaseUrls.meetings + '/' + meetingId;
+			meetingRef = new Firebase(meetingUrl);
+
+			return $firebaseArray(meetingRef);
+		},
+		attend : function(name, then) {
+			meetingRef.$add({
+				name: name,
+				status: 'ontime'
+			}).then(then);
+		},
+		setStatus : function(id, status) {
+
+		}
 	}
-	$scope.newMeeting();
-
 })
-.controller('meetingCtrl', function ($scope, $routeParams, $firebaseObject, $firebaseArray, $cookieStore, IdentityService) {
-	$scope.meeting.id = $routeParams.meetingId;
+.controller('meCtrl', function ($scope, meService) {
+	$scope.me = meService;
+})
+.directive('me', function() {
+  return {
+    templateUrl: 'partials/me.html'
+  };
+})
+.controller('homeCtrl', function ($scope, meetingService) {
+	$scope.meetingId = randomString(10);
+})
+.controller('meetingCtrl', function ($scope, $routeParams, meetingService) {
+	$scope.meetingId = $routeParams.meetingId;
 	$scope.currentUrl = encodeURIComponent(window.location);
 	
 	console.log($routeParams.meetingId);
 
-	var meetingUrl = chairFirebaseUrls.meetings + '/' + $routeParams.meetingId;
-	var meetingRef = new Firebase(meetingUrl);
-	$scope.attendance = $firebaseArray(meetingRef);
-	
-	// $scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings)
-	// 	.child($routeParams.meetingId))
-	// 	.$asArray();
-
-	$scope.me;
-	$scope.attend = function() {
-
-		// console.log($scope.me);
-
-		// if (!$scope.me) {
-
-			// Add me to the meeting
-			$scope.attendance.$add($scope.user).then(function(ref) {
-
-				console.log(ref);
-
-				// Save the reference
-				$scope.me = ref;
-				// $scope.user.id = ref.key();
-
-				var userUrl = meetingUrl + '/' + ref.key();
-				var userRef = new Firebase(userUrl);
-				var obj = $firebaseObject(userRef);
-				obj.$bindTo($scope, "user").then(function(ref) {
-					console.log(ref);
-				  // console.log($scope.data); // { foo: "bar" }
-				  // $scope.data.foo = "baz";  // will be saved to Firebase
-				  // ref.set({ foo: "baz" });  // this would update Firebase and $scope.data
-				});
-			});
-		// }
-	};
-	// $scope.attend = function() {
-	// 	if (!$scope.me)
-	// 		$scope.attendance.$add($scope.user.name).then(function(ref) {
-	// 			$scope.me = ref;
-	// 			$scope.user.id = ref.key();
-	// 			// $scope.setUser();
-	// 		});
-	// }
-})
-.controller('roomCtrl', function ($scope, $routeParams, $firebase, $cookieStore, IdentityService) {
-	$scope.meeting.id = $routeParams.meetingId;
-	$scope.currentUrl = encodeURIComponent(window.location);
-
-	$scope.attendance = $firebase(new Firebase(chairFirebaseUrls.meetings).child($routeParams.meetingId))
-	.$asArray();
+	$scope.attendance = meetingService.get($scope.meetingId);
 })
 .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'homeCtrl'
-      }).
-      when('/chair', {
-        templateUrl: 'partials/chair.html',
-        controller: 'chairCtrl'
-      }).
-      when('/:meetingId', {
-        templateUrl: 'partials/meeting.html',
-        controller: 'meetingCtrl'
-      }).
-      when('/meeting/:meetingId', {
-        templateUrl: 'partials/meeting.html',
-        controller: 'meetingCtrl'
-      }).
-      when('/meeting/:meetingId/room', {
-        templateUrl: 'partials/room.html',
-        controller: 'roomCtrl'
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-  }]);
+	function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: 'partials/home.html',
+				controller: 'homeCtrl'
+			}).
+			when('/:meetingId', {
+				templateUrl: 'partials/meeting.html',
+				controller: 'meetingCtrl'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+	}]);
 
 
 function randomString(length) {
-  var text = "";
-  var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
-  // var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	length = length || 10;
 
-  for(var i=0; i < length; i++)
+	var text = "";
+	var possible = "abcdefghijklmnopqrstuvwxyz";
+	// var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+	// var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for(var i=0; i < length; i++)
 	text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  return text;
+	return text;
 }
---
+++ readme.md
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+I am a Chair
+- Open website
+- click Chair
+- see attendees appear in real time
+- update attendee status
+	- on time
+	- late
+- add no shows
+
+
+I am an attendant with a QR reader
+- scan QR
+- website loads asks my name
+- loads list of attendants
+
+
+I am an attendant without a QR reader
+- enter url
+- (same as above)
+
+
+
+
+
+
+page components
+
+home
+- me
+- chair button
+- attend input
+
+meeting without name
+- me
+
+meeting after input name
+- meeting
+	- qr
+- attendees
+
+
+
+Services
+
+me
+- get / set my name
+
+meeting
+- attend a meeting
+
+
